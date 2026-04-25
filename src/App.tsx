cd6ec1bec7f7e5ce1bce7ae6d2f9a3357f1ee705
Abhiseck Bhattacharya <abhiseck@outlook.com>
2026-04-25 07:40:26 +0530

    feat: update dependencies and add new UI components
    - Added new dependencies: @fontsource-variable/geist, class-variance-authority, clsx, lucide-react, radix-ui, shadcn, tailwind-merge, and tw-animate-css.
    - Refactored App component to include a new Button component.
    - Updated global styles in index.css with new theme variables and imports.
    - Configured TypeScript paths for easier imports.
    - Enhanced Vite configuration with path aliasing.
    - Created a new components.json file for shadcn configuration.
    - Implemented a reusable Button component with variants and sizes.
    - Added utility functions for class name management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button variant={"outline"}>Click me</Button>
+      </div>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -13,5 +14,10 @@ export default defineConfig({
   server: {
     port: 3055, // Sets the fixed port
     open: true, // Automatically opens the app in your default browser
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
