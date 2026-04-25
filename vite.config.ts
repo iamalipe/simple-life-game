fedba066e8c5bac5f19e8ed5aaf4ffbb66c230c0
Abhiseck Bhattacharya <abhiseck@outlook.com>
2026-04-25 07:27:49 +0530

    feat: integrate Tailwind CSS and remove legacy styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+  server: {
+    port: 3055, // Sets the fixed port
+    open: true, // Automatically opens the app in your default browser
+  },
+});
